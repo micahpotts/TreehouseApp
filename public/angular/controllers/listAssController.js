3eedd6627e193e70576b029acf30290847fc689b
Micah Potts <mrs.micahpotts@gmail.com>
2017-03-27 16:30:21 -0400

    Working on delete button

--- public/angular/controllers/listAssController.js
+++ public/angular/controllers/listAssController.js
@@ -18,6 +18,24 @@ myApp.controller("ListAssignmentsController", [
       function(response) {
         alert("Problem in ListAssignmentsController");
       }
-    )
+    );
+
+    $scope.deleteAss = function(id) {
+      $http({
+        method: "DELETE",
+        url: "http://localhost:3000/assignments/${id}"
+      })
+        .then(
+          function(response) {
+            $scope.sata = response.data;
+            $scope.status = response.status;
+            $route.reload();
+          },
+          function(response) {
+            $scope.dats = response.data || "Request failed";
+            $scope.status = response.status;
+          }
+        );
+    };
   }
-])
+]);
